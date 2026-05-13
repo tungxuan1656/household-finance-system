60c0be48fc8b540078e6aa1fd400627e3ad8452f
Tùng <tungxuan101998@gmail.com>
2026-05-13 17:41:09 +0700

    Adds a reusable card placeholder component

---
+++ apps/web/src/components/shared/card-placeholder.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+
+type CardPlaceholderProps = React.ComponentProps<typeof Card> & {
+  isLoading?: boolean
+  isEmpty?: boolean
+  title?: string
+  description?: string
+}
+
+function CardPlaceholder({
+  isLoading,
+  isEmpty,
+  children,
+  title,
+  description,
+  ...props
+}: CardPlaceholderProps) {
+  if (isLoading) {
+    return (
+      <Card {...props}>
+        <CardContent>
+          <div className='flex flex-col gap-4'>
+            <Skeleton className='h-4 w-3/4' />
+            <Skeleton className='h-4 w-1/2' />
+          </div>
+        </CardContent>
+      </Card>
+    )
+  }
+
+  if (isEmpty) {
+    return (
+      <Card {...props}>
+        <CardHeader className='text-center'>
+          <CardTitle>{title ?? 'Không có dữ liệu'}</CardTitle>
+          <CardDescription>
+            {description ??
+              'Không có dữ liệu mà bạn đang tìm kiếm, vui lòng thử lại sau'}
+          </CardDescription>
+        </CardHeader>
+      </Card>
+    )
+  }
+
+  return <>{children}</>
+}
+
+export { CardPlaceholder }
