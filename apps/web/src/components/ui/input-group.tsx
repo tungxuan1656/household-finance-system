106433a25ede41a772cc69ce4b965f29de918e67
Tùng <tungxuan101998@gmail.com>
2026-05-15 14:22:56 +0700

    feat: add size variant support to Input component and update InputGroup accordingly

--- apps/web/src/components/ui/input-group.tsx
+++ apps/web/src/components/ui/input-group.tsx
@@ -113,6 +113,7 @@ function InputGroupText({ className, ...props }: React.ComponentProps<'span'>) {
 
 function InputGroupInput({
   className,
+  size: _size,
   ...props
 }: React.ComponentProps<'input'>) {
   return (
